69c660863bf0035a3dc8c4769a034e388dbf4670
jhozasa000 <jhozasa@hotmail.com>
2023-09-03 02:07:38 -0500

    modulo calendario y jornada

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors'); 
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var calendarRouter = require('./routes/calendario');
+var jornadaRouter = require('./routes/jornada');
+
 
 var app = express();
 
@@ -15,12 +21,19 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({
+  origin: '*'
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/calendario', calendarRouter);
+app.use('/jornada', jornadaRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/login.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+require('dotenv').config();
+
+const hostname = process.env.DB_HOST;
+const database = process.env.DB_DATABASE;
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASSWORD;
+  
+// se crea la conexion 
+var pool        = mysql.createPool({
+  connectionLimit : 10, // default = 10
+  host            : hostname,
+  user            : user,
+  password        : pass,
+  database        : database
+});
+
+/* get user login. */
+router.get('/', function (req, res) {
+  pool.getConnection(function (err, connection) {
+      connection.query(`SELECT user,id,level FROM login WHERE user = '${req.query.user}'  and pass='${req.query.pass}' and state = 1` , function (err, rows) {
+          connection.release();
+          if (err) throw err;
+          res.send(JSON.stringify(rows));
+      });
+  });
+});
+
+/* post user login. */
+router.post('/', function (req, res) {
+  pool.getConnection(function (err, connection) {
+      connection.query(`SELECT user,id,level FROM login WHERE user = '${req.body.user}'  and pass='${req.body.pass}' and state = 1` , function (err, rows) {
+          connection.release();
+          if (err) throw err;
+          res.send(JSON.stringify(rows));
+      });
+  });
+});
+
+
+module.exports = router;
