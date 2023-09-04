948e58fb45f781050ad803903b8e8599786c541f
jhozasa000 <jhozasa@hotmail.com>
2023-09-04 18:24:31 -0500

    modificacion docentes

--- routes/docentes.js
+++ routes/docentes.js
@@ -20,13 +20,27 @@ var pool        = mysql.createPool({
   database        : database
 });
 
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+   // cb(null, "./public/files");
+    cb(null, "/Users/jhonnatanzapata/React/softeduapp/public/assets/files");
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split("/")[1];
+    cb(null, `teacherfile-${file.fieldname}-${Date.now()}.${ext}`);
+  },
+});
+
+const upload = multer({
+  storage: multerStorage,
+});
+
 
 /* insertar docentes */
-router.post('/insert', function (req, res) {
+router.post('/insert',upload.single('files'), function (req, res) {
     pool.getConnection(function (err, connection) {
       const data = req.body;
-
-        connection.query(  `INSERT INTO docentes(name,numberid,profession,telephone,address,files) VALUES('${data.name}',${data.numberid},${data.profession},${data.telephone},'${data.address}','${data.files}') ` , function (err, rows) {
+       connection.query(  `INSERT INTO docentes(name,numberid,profession,telephone,address,files) VALUES('${data.name}',${data.numberid},${data.profession},${data.telephone},'${data.address}','${req?.file?.filename??''}') ` , function (err, rows) {
             connection.release();
             if (err) throw err;
             res.send(JSON.stringify(rows));
@@ -36,7 +50,7 @@ router.post('/insert', function (req, res) {
 
 router.post('/select', function (req, res) {
     pool.getConnection(function (err, connection) {
-        connection.query(`SELECT numberid FROM docentes WHERE numberid = '${req.body.numberid} ' ` , function (err, rows) {
+        connection.query(`SELECT numberid FROM docentes WHERE numberid = ${req.body.numberid}` , function (err, rows) {
             connection.release();
             if (err) throw err;
             res.send(JSON.stringify(rows));
@@ -46,7 +60,7 @@ router.post('/select', function (req, res) {
 
 router.get('/select', function (req, res) {
     pool.getConnection(function (err, connection) {
-        connection.query(`SELECT name,numberid,profession,telephone,address,files FROM docentes WHERE state = 1` , function (err, rows) {
+        connection.query(`SELECT doc.id id,doc.name,doc.numberid,pro.name profession,pro.id idpro,doc.telephone,doc.address,doc.files FROM  docentes doc INNER JOIN profesion pro ON doc.profession = pro.id WHERE doc.STATE = 1` , function (err, rows) {
             connection.release();
             if (err) throw err;
             res.send(JSON.stringify(rows));
@@ -67,10 +81,11 @@ router.get('/select', function (req, res) {
   
   });
 
-  router.put('/edit', function(req, res) {
+  router.put('/edit',upload.single('files'), function(req, res) {
     const data = req.body;
+    let imgNew = req?.file?.filename??data.filesbd
     pool.getConnection(function (err, connection) {
-      connection.query(`UPDATE docentes SET name = '${data.name}',numberid = ${data.numberid},profession = ${data.profession},telephone = ${data.telephone},address = '${data.address}',files = '${data.files}' WHERE id= ${data.id}` , function (err, rows) {
+      connection.query(`UPDATE docentes SET name = '${data.name}',profession = ${data.profession},telephone = ${data.telephone},address = '${data.address}',files = '${imgNew}' WHERE id= ${data.id}` , function (err, rows) {
           connection.release();
           if (err) throw err;
           res.send(JSON.stringify(rows));
